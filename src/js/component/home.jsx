573243597f1be2030f4a59caef7f8cce23321f07
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-07-25 00:55:36 +0000

    not yet

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [selectColor, SetSelectColor] = useState("");
+	const [selectRedColor, setSelectRedColor] = useState(false);
+	const [selectYellowColor, setSelectYellowColor] = useState(false);
+	const [selectGreenColor, setSelectGreenColor] = useState(false);
 
 	return (
 			<div>
 				<div className="traffic-top container">h</div>
 				<div className="container">
 					<div className="traffic-body container">
-						<div onClick={() => SetSelectColor('red')} 
-						className={'red' + (selectColor === 'red' ? "glow" : "")}>
-						</div>
-						<div onClick={() => SetSelectColor('yellow')} 
-						className={(selectColor === 'yellow' ? "glow" : "")}>
-						</div>
-						<div onClick={() => SetSelectColor('green')} 
-						className={'green' + (selectColor === 'green' ? "glow" : "")}>
-						</div>
+						<div className={"glow" + (selectRedColor ? "lightred" : "")} onClick={() =>{setSelectRedColor(true); setSelectGreenColor(false); setSelectYellowColor(false)}}></div>
+						<div className={"glow" + (selectYellowColor ? "lightyellow" : "")} onClick={() =>{setSelectYellowColor(true); setSelectGreenColor(false); setSelectRedColor(false)}}></div>
+						<div className={"glow" + (selectGreenColor ? "lightgreen" : "")} onClick={() =>{setSelectGreenColor(true); setSelectRedColor(false); setSelectYellowColor(false)}}></div>
 					</div>
 				</div>
 			</div>
